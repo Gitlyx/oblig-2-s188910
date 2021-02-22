a9ecd9f6dbd555cc8d2eff0cd6a9b9466f3a26fe
Charlie Vo <charlievo22@gmail.com>
2021-02-22 14:19:47 +0100

    sectioned out test module

--- src/main/java/oslomets188910/gitlyxprojects/HeiController.java
+++ src/main/java/oslomets188910/gitlyxprojects/HeiController.java
@@ -2,12 +2,14 @@ package oslomets188910.gitlyxprojects;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+/*
 @RestController
 public class HeiController {
-    @GetMapping("/")
+    @GetMapping("//")
     public String hei(String navn) {
         return "Hei verden" + navn;
 
     }
 }
+
+ */
